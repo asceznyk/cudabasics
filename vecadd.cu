63d099b634032f034b4471a3e7560fd6f3d57f3d
asceznyk <asceznyk@gmail.com>
2021-08-30 11:49:01 +0530

    ..

--- vecadd.cu
+++ vecadd.cu
@@ -3,8 +3,21 @@
 #include <iostream>
 #include <vector>
 
+void print_array(int arr[], int m) {
+  for (int i = 0; i < m; i++)
+    printf("%d", arr[i]);
+}
+
+void init_array(int arr[], int m) {
+  for (int i = 0; i < m; i++)
+    arr[i] = rand() % 100;
+}
+
 int main() {
   int n = 10;
   int *a = new int[n];
+
+  init_array(a, n);
+  print_array(a, n);
 }
 
