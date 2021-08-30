e4942f9369a8614b76d43747d52660b4be8e2124
asceznyk <asceznyk@gmail.com>
2021-08-30 19:43:02 +0530

    oh my god

--- conv2d.cu
+++ conv2d.cu
@@ -62,7 +62,7 @@ void verify_result(int *matrix, int *result, int *mask, int N) {
     }
   }
 
-  print("result[%d] = %d == %d = temp \n", N-1, result[N-1], temp);
+  printf("result[%d] = %d == %d = temp \n", N-1, result[N-1], temp);
 }
 
 int main() {
