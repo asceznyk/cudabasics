fa70376acfefa6a917c18f15b671481417b52360
asceznyk <asceznyk@gmail.com>
2021-08-30 12:28:00 +0530

    ..

--- vecadd.cu
+++ vecadd.cu
@@ -13,11 +13,24 @@ void init_array(int arr[], int m) {
     arr[i] = rand() % 100;
 }
 
+void add_array(int a[], int b[], int m[]) {
+  for (int i = 0; i < m; i ++)
+    b[i] = a[i] + b[i];
+}
+
 int main() {
   int n = 10;
   int *a = new int[n];
+  int *b = new int[n];
 
   init_array(a, n);
+  init_array(b, n);
+
   print_array(a, n);
+  printf("\n");
+  print_array(b, n);
+  printf("\n");
+  add_array(a, b, n);
+  print_array(b, n);
 }
 
