61728dd08e0cf751daa4596abdbff1777b66d1e4
asceznyk <asceznyk@gmail.com>
2021-09-04 11:50:23 +0530

    baisc c matmul

--- mmul.cu
+++ mmul.cu
@@ -9,19 +9,20 @@ void print_matrix(int *m, int y, int x) {
       printf("%d, ", m[i * x + j]);
     printf("]\n");
   }
+  printf("\n");
 }
 
 void init_matrix(int *m, int l) {
   for (int i = 0; i < l; i++) {
-    m[i] = rand() % 100;
+    m[i] = 10; //rand() % 100;
   }
 }
 
 void matmul(int *a, int *b, int *c, int l, int m, int n) {
   for(int i = 0; i < l; i++) {
-    for(int j = 0; j < m; j++) { 
-      for(int k = 0; k < n; k++) {
-        c[i * n + j] += a[i * m + k] * b[k + j * n]; //c[i][j] += a[i][k] * b[k][j]
+    for(int j = 0; j < n; j++) { 
+      for(int k = 0; k < m; k++) {
+        c[i * n + j] += a[i * m + k] * b[k + j * m]; //c[i][j] += a[i][k] * b[k][j]
       }
     }
   }
